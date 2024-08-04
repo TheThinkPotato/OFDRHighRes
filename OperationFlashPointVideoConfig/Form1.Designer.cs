fc398adbc3f8b4fdc2c0efdd167e362b7ff3c646
Dan Lopez <86757951+TheThinkPotato@users.noreply.github.com>
2024-08-04 22:26:40 +1000

    First Commiet

---
+++ OperationFlashPointVideoConfig/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace OperationFlashPointVideoConfig
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBoxRes = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxRefresh = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBoxFullscreen = new System.Windows.Forms.CheckBox();
+            this.checkBoxVsync = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxRes
+            // 
+            this.comboBoxRes.FormattingEnabled = true;
+            this.comboBoxRes.Location = new System.Drawing.Point(108, 28);
+            this.comboBoxRes.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.comboBoxRes.Name = "comboBoxRes";
+            this.comboBoxRes.Size = new System.Drawing.Size(223, 23);
+            this.comboBoxRes.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Set Resolution:";
+            // 
+            // comboBoxRefresh
+            // 
+            this.comboBoxRefresh.FormattingEnabled = true;
+            this.comboBoxRefresh.Location = new System.Drawing.Point(108, 61);
+            this.comboBoxRefresh.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.comboBoxRefresh.Name = "comboBoxRefresh";
+            this.comboBoxRefresh.Size = new System.Drawing.Size(66, 23);
+            this.comboBoxRefresh.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Set Refresh:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(185, 108);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 48);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBoxFullscreen
+            // 
+            this.checkBoxFullscreen.AutoSize = true;
+            this.checkBoxFullscreen.Location = new System.Drawing.Point(24, 108);
+            this.checkBoxFullscreen.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBoxFullscreen.Name = "checkBoxFullscreen";
+            this.checkBoxFullscreen.Size = new System.Drawing.Size(88, 19);
+            this.checkBoxFullscreen.TabIndex = 3;
+            this.checkBoxFullscreen.Text = "Full Screen";
+            this.checkBoxFullscreen.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxVsync
+            // 
+            this.checkBoxVsync.AutoSize = true;
+            this.checkBoxVsync.Location = new System.Drawing.Point(24, 137);
+            this.checkBoxVsync.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBoxVsync.Name = "checkBoxVsync";
+            this.checkBoxVsync.Size = new System.Drawing.Size(57, 19);
+            this.checkBoxVsync.TabIndex = 4;
+            this.checkBoxVsync.Text = "Vsync";
+            this.checkBoxVsync.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 65);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::OperationFlashPointVideoConfig.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(344, 171);
+            this.Controls.Add(this.checkBoxVsync);
+            this.Controls.Add(this.checkBoxFullscreen);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxRefresh);
+            this.Controls.Add(this.comboBoxRes);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Gold;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "OFDR High Res Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBoxRes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxRefresh;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBoxFullscreen;
+        private System.Windows.Forms.CheckBox checkBoxVsync;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ OperationFlashPointVideoConfig/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace OperationFlashPointVideoConfig
+{
+    public partial class Form1 : Form
+    {
+        string myDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        string ofdrPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\My Games\OFDR\hardwaresettings";
+
+        string hardwareSettingsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\My Games\OFDR\hardwaresettings\hardware_settings_info.xml";
+        string configSettingsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\My Games\OFDR\hardwaresettings\hardware_settings_config.xml";
+
+        private List<DisplayMode> displayModes = new List<DisplayMode>();
+        private List<int> refreshRates = new List<int>();
+
+        private DisplayMode currentResoltion;
+        private int currentRefreshRate;
+        private bool fullscreen = false;
+        private bool vsync = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            detectResolutions();
+            getRefreshRates();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            syncSettings();
+        }
+
+        private void syncSettings()
+        {
+            try
+            {
+                getSettings();
+                syncGui();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void syncGui()
+        {
+            comboBoxRes.SelectedIndex = displayModes.FindIndex(dm => dm.width == currentResoltion.width && dm.height == currentResoltion.height);
+            comboBoxRefresh.SelectedIndex = refreshRates.FindIndex(rr => rr == currentRefreshRate);
+            checkBoxFullscreen.Checked = fullscreen;
+            checkBoxVsync.Checked = vsync;
+        }
+
+        private void getSettings()
+        {
+            var xmlDoc = XDocument.Load(configSettingsPath);
+
+            var graphicsCard = xmlDoc.Root.Element("graphics_card");
+            var resolution = graphicsCard?.Element("resolution");
+
+            if (resolution != null)
+            {
+                // Read attributes from resolution
+                int width = (int)resolution.Attribute("width");
+                int height = (int)resolution.Attribute("height");
+                currentResoltion = new DisplayMode(width, height);
+
+                fullscreen = (string)resolution.Attribute("fullscreen") == "1";
+                vsync = (string)resolution.Attribute("vsync") == "1";
+
+                // Read refresh rate
+                var refreshRate = resolution.Element("refreshRate");
+                currentRefreshRate = (int)refreshRate.Attribute("rate");
+            }
+        }
+
+        private void saveSettings()
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(configSettingsPath);
+
+                XmlNode resolutionNode = doc.SelectSingleNode("//resolution");
+                XmlNode refreshNode = doc.SelectSingleNode("//refreshRate");
+
+                if (resolutionNode != null)
+                {
+                    // Update attributes
+                    XmlElement resolutionElement = (XmlElement)resolutionNode;
+                    resolutionElement.SetAttribute("width", displayModes[comboBoxRes.SelectedIndex].width.ToString());  // New width
+                    resolutionElement.SetAttribute("height", displayModes[comboBoxRes.SelectedIndex].height.ToString()); // New height
+                    resolutionElement.SetAttribute("aspect", displayModes[comboBoxRes.SelectedIndex].aspectRatio); // New aspect ratio
+                    resolutionElement.SetAttribute("fullscreen", checkBoxFullscreen.Checked ? "1" : "0"); // New fullscreen setting
+                    resolutionElement.SetAttribute("vsync", checkBoxVsync.Checked ? "1" : "0"); // New vsync setting
+
+
+                    XmlElement refreshElement = (XmlElement)refreshNode;
+                    refreshElement.SetAttribute("rate", refreshRates[comboBoxRefresh.SelectedIndex].ToString()); // New refresh rate setting
+
+                    // Save the changes back to the file
+                    doc.Save(configSettingsPath);
+                }
+                else
+                {
+                    MessageBox.Show("Resolution element not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void getRefreshRates(bool debug = false)
+        {
+            try
+            {
+                XDocument xDoc = XDocument.Load(hardwareSettingsPath);
+
+                List<int> readRefreshRates = xDoc.Descendants("refreshRate")
+                                 .Select(rrr => (int)rrr.Attribute("rate"))
+                                 .Distinct()
+                                 .OrderByDescending(rrr => rrr)
+                                 .ToList();
+
+                refreshRates = readRefreshRates;
+
+                comboBoxRefresh.Items.Clear();
+                comboBoxRefresh.DataSource = refreshRates;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void detectResolutions(bool debug = false)
+        {
+            var screens = Screen.AllScreens;
+            List<DisplayMode> detectedDisplayModes = new List<DisplayMode>();
+            foreach (var screen in screens)
+            {
+                // Get the current resolution
+                var bounds = screen.Bounds;
+                detectedDisplayModes.Add(new DisplayMode(bounds.Width, bounds.Height));
+            }
+
+
+            List<DisplayMode> extraDisplayModes = new List<DisplayMode>();
+
+            //add extra resolutions
+            foreach (var mode in detectedDisplayModes)
+            {
+                List<DisplayMode> newModes = new List<DisplayMode>();
+
+                newModes.Add(new DisplayMode((int)(mode.width * 0.75), (int)(mode.height * 0.75)));
+                newModes.Add(new DisplayMode((int)(mode.width * 0.5), (int)(mode.height * 0.5)));
+
+                foreach (var newMode in newModes)
+                {
+                    if (newMode.width > 1280 && newMode.height > 1024)
+                    {
+                        extraDisplayModes.Add(newMode);
+                    }
+                }
+            }
+
+            detectedDisplayModes.AddRange(extraDisplayModes);
+
+            //removed doubles up in the list
+            displayModes = detectedDisplayModes
+                .Select(dm => new { dm.width, dm.height })
+                .Distinct()
+                .Select(dm => new DisplayMode(dm.width, dm.height))
+                .OrderByDescending(dm => dm.width)
+                .ThenByDescending(dm => dm.height)
+                .ToList();
+
+            comboBoxRes.Items.Clear();
+            comboBoxRes.DataSource = displayModes;
+        }
+
+        private void detectFiles()
+        {
+            string[] files = System.IO.Directory.GetFiles(ofdrPath, "*.xml");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveSettings();
+        }
+    }
+
+    public class DisplayMode
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public string aspectRatio
+        {
+            get
+            {
+                return ((double)width / (double)height).ToString("F7").Substring(0, 8);
+            }
+        }
+
+        public DisplayMode(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{width}x{height} ({getRatioForDisplay()})";
+        }
+
+        private string getRatioForDisplay()
+        {
+            switch (aspectRatio)
+            {
+                case "1.333333":
+                    return "4:3";
+                case "1.777777":
+                    return "16:9";
+                case "1.600000":
+                    return "16:10";
+                default:
+                    return "unknown";
+            }
+        }
+
+    }
+}
